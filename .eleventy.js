482d369e648c86c731ef1feace3891bda3ddda5c
Matt DeCamp <matthewdecamp@gmail.com>
2022-03-10 10:39:49 -0500

    initial eleventy scripting
    1. Clean CSS filter
    2. Passthroughs
    3. Quiet Mode: On
    4. Watch target for styles folder
    5. HTML min transforms
    6. SVG sprite plugin
    7. Directory, template formats, engine, and markdown assignments.

---
+++ .eleventy.js
@@ -0,0 +1,44 @@
+const CleanCSS = require("clean-css");
+const svgSprite = require("eleventy-plugin-svg-sprite");
+
+module.exports = function (eleventyConfig) {
+  // Shh!
+  eleventyConfig.setQuietMode(true);
+  // Passthroughs
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  // Watch these on build
+  eleventyConfig.addWatchTarget("./src/styles/");
+  // TRANSFORMS //
+  // minify HTML
+  const htmlMinTransform = require("./src/transforms/html-min.js");
+  const isProduction = process.env.ELEVENTY_ENV === "production";
+  // html min only in production
+  if (isProduction) {
+    config.addTransform("htmlmin", htmlMinTransform);
+  }
+  // FILTERS //
+  // Clean CSS and minify
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  // PLUGINS //
+  // SVG Sprite
+    eleventyConfig.addPlugin(svgSprite, {
+      path: "./src/assets/svg", // relative path to SVG directory
+      // (MUST be defined when initialising plugin)
+    });
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: "includes",
+      data: "data",
+    },
+    templateFormats: ["html", "njk", "md", "11ty.js"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+  };
+};
